4984413d736a50933a294bb33d55ddc64f84cfed
unreal3579 <unreal3579@bk.ru>
2024-05-29 09:35:04 +0300

    Create fox_ziyi.mk

---
+++ fox_ziyi.mk
@@ -0,0 +1,40 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2024 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+# screen settings
+OF_SCREEN_H := 2400
+OF_STATUS_H := 99
+OF_STATUS_INDENT_LEFT := 48 
+OF_STATUS_INDENT_RIGHT := 48
+OF_CLOCK_POS := 1
+OF_HIDE_NOTCH := 1
+
+# other stuff
+OF_USE_GREEN_LED := 0
+OF_QUICK_BACKUP_LIST := "/boot;/data;"
+
+# default keymaster service version
+OF_DEFAULT_KEYMASTER_VERSION := 4.1
+
+# A/B with recovery partition
+OF_AB_DEVICE_WITH_RECOVERY_PARTITION := 1
+
+# lz4 compression
+OF_USE_LZ4_COMPRESSION := 1 
