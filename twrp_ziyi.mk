9cce92eae50686c05db043f8f4e0e2b0e12c02df
unreal3579 <unreal3579@bk.ru>
2024-05-31 22:18:08 +0300

    Update twrp_ziyi.mk

--- twrp_ziyi.mk
+++ twrp_ziyi.mk
@@ -7,7 +7,7 @@ PRODUCT_RELEASE_NAME := ziyi
 DEVICE_PATH := device/xiaomi/$(PRODUCT_RELEASE_NAME)
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
