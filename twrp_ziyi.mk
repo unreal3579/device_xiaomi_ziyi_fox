43fd2128275379466cbbfdc5f125ca2db561fd6f
unreal3579 <unreal3579@bk.ru>
2024-06-06 13:17:53 +0300

    Update

--- twrp_ziyi.mk
+++ twrp_ziyi.mk
@@ -1,23 +1,26 @@
 #
-# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2023 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-PRODUCT_RELEASE_NAME := ziyi
-DEVICE_PATH := device/xiaomi/$(PRODUCT_RELEASE_NAME)
 
-# Inherit from those products. Most specific first.
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from those products
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
-# Inherit from ziyi device
+# Inherit from device configuration
 $(call inherit-product, device/xiaomi/ziyi/device.mk)
+# Inherit from TWRP common configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Fox settings
 $(call inherit-product-if-exists, device/xiaomi/ziyi/fox_ziyi.mk)
+
+
+PRODUCT_RELEASE_NAME := ziyi
 
 PRODUCT_DEVICE := ziyi
 PRODUCT_NAME := twrp_ziyi
@@ -26,12 +29,3 @@ PRODUCT_MODEL := 2210129SG
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Assert
-#TARGET_OTA_ASSERT_DEVICE := ziyi
-
-# Theme
-#TW_STATUS_ICON_ALIGN := center
-#TW_Y_OFFSET :=
-#TW_H_OFFSET :=
-
